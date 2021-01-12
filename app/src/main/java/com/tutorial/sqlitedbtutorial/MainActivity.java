b33883da237b001126b3249f5352b15487466f92
Shiv Rana <shivkumar.r@purplle.com>
2021-01-12 21:33:10 +0530

    initial commit

---
+++ app/src/main/java/com/tutorial/sqlitedbtutorial/MainActivity.java
@@ -0,0 +1,79 @@
+package com.tutorial.sqlitedbtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.tutorial.sqlitedbtutorial.db.MyDatabaseHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper myDatabaseHelper;
+    private String name;
+    private String phone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btReadData = findViewById(R.id.bt_read_data);
+        Button btWriteData = findViewById(R.id.bt_write_data);
+        EditText etName = findViewById(R.id.et_name);
+        EditText etPhone = findViewById(R.id.et_phone);
+
+
+        myDatabaseHelper = new MyDatabaseHelper(this);
+
+        btWriteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = etName.getText().toString();
+                phone = etPhone.getText().toString();
+                insertIntoDb(name, phone);
+            }
+        });
+
+
+        btReadData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readData();
+            }
+        });
+
+    }
+
+
+    private void insertIntoDb(String name, String phone) {
+
+        SQLiteDatabase writableDb = myDatabaseHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(MyDatabaseHelper.NAME, name);
+        values.put(MyDatabaseHelper.PHONE, phone);
+        writableDb.insert(MyDatabaseHelper.TABLE_NAME, null, values);
+        Toast.makeText(this, "Database written successfully", Toast.LENGTH_LONG).show();
+    }
+
+    private void readData() {
+        String selectQuery = "SELECT  * FROM " + MyDatabaseHelper.TABLE_NAME;
+        SQLiteDatabase db = myDatabaseHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                // get the data into array, or class variable
+                String name = cursor.getString(1);
+                String phone = cursor.getString(2);
+                Log.d("MyDatabaseData", "name-> " + name + ", " + "Phone-> " + phone);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+    }
+}
---
+++ app/src/main/java/com/tutorial/sqlitedbtutorial/db/MyDatabaseHelper.java
@@ -0,0 +1,44 @@
+package com.tutorial.sqlitedbtutorial.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.net.IDN;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    public static final String TABLE_NAME = "USER_TABLE";
+    public static final String ID = "_id";
+    public static final String NAME = "name";
+    public static final String PHONE = "phone";
+    public static final String DATABASE_NAME = "my_db";
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(getCommand());
+    }
+
+    private String getCommand() {
+        String command = "CREATE TABLE " + TABLE_NAME + " ("
+                + ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + NAME + " TEXT NOT NULL,"
+                + PHONE + " TEXT"
+                + ");";
+        return command;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        Log.w("TAG", "Upgrading database from version " + oldVersion + " to "
+                + newVersion + ", which will destroy all old data");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
